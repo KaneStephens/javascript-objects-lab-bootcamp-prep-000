2a6a84600753e652eb1b8ac3e8914c9d96378439
Kane Pangalia Stephens <kanepstephens@gmail.com>
2019-07-14 18:51:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var recipes = { foo: 'bar'}
 
-
+function updateObjectWithKeyAndValue(object, key, value){
+  newObject = object.assign({}, )
+}
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
